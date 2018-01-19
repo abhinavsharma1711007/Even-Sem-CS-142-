ce2e147e6e90b77f35741edcd482a1f3caec6f4a
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2018-01-19 22:41:59 +0530

    Update Question1.cpp

--- LAB 2/Question1.cpp
+++ LAB 2/Question1.cpp
@@ -1,138 +1,156 @@
 #include<iostream>
-
 using namespace std;
 
 class node
-{  public :
-    int data;
-    node *next;
+{
+public:
+	int element;
+	node *next;
 };
 
-class link
+class list
 {
-    node *head;
-    node *tail;
+private:
+	node *head;
+	node *end;
+public:
+	list()
+	{
+		head = NULL;
+		end = NULL;
+	}
 
-    link ()
-    {
-      head = NULL;
-      tail = NULL;
-    }
-    public :
-       void insert_list(int data);
-       void insert_at(int pos, int data);
-       void delete_list();
-       void delete_at(int pos);
-       void display_l();
-       void count_l();
+	void insert(int data)
+	{
+		node *temp = new node;
+		temp->element = data;
+		if(head == NULL)
+		{
+			head=temp;
+			end=temp;
+			temp->next=NULL;
+		}
+		else
+		{
+			end->next=temp;
+			temp->next=NULL;
+			end = temp;
+		}
+	}
+
+	void insertat(int pos, int data)
+	{
+		node *temp = new node;
+		node *past = new node;
+		past = head;
+		temp->element=data;
+		for(int i=2;i<pos;i++)
+		{
+			past=past->next;
+		}
+		temp->next = past->next;
+		past->next = temp;
+	}
+
+	void del()
+	{
+		node *current=new node;
+		node *previous=new node;
+		current=head;
+		while(current->next!=NULL)
+		{
+			previous=current;
+			current=current->next;	
+		}
+		end=previous;
+		previous->next=NULL;
+		delete current;
+	}
+
+	void deleteat(int pos)
+	{
+  		node *current=new node;
+  		node *previous=new node;
+  		current=head;
+  		for(int i=1;i<pos;i++)
+  		{
+  			previous=current;
+  			current=current->next;
+  		}
+  		previous->next=current->next;
+  	}
+
+  	void countItems()
+  	{
+  		int n=0;
+  		node *temp= new node;
+  		temp = head;
+  		while(temp!=NULL)
+  		{
+  			n++;
+  			temp=temp->next;
+  		}
+  		cout << n;
+  	}
+
+  	void display()
+  	{
+  		node *temp=new node;
+  		temp=head;
+  		while(temp!=NULL)
+  		{
+  			cout << temp->element<<" ->";
+  			temp = temp->next;
+  		}
+  		cout << "NULL";
+  	}
 };
 
 int main()
-{ char ch;
-  cout<<"Choose what you want to do : "<<endl;
-  cout<<"(a) create a linked list."<<endl;
-  cout<<"(b) insert data in linked list."<<endl;
-  cout<<"(c) insert a data at any position. "<<endl;
-  cout<<"(d) delete the linked list. "<<endl;
-  cout<<"(e) delete data of a specific position."<<endl;
-  cout<<"(f) display the elements of linked list."<<endl;
-  cout<<"(g) count the number of elements in the list."<<endl;
-  cin>> ch;
-     if (ch=='a')
-     {
-        cout<<"Linked list created."<<endl;
-     }
-     else if (ch=='b')
-     {
-         int num;
-         link in_l;
-         cout<<"How many data do you want to add ?"<<endl;
-         cin>>num;
-         for(int i=1;i<=num;i++)
-         {   int data;
-             cout<<"Data "<<i<<" = ";
-             cin >> data;
-             in_l.insert_list(data);
-         }
-     }
-
+{
+	int value,position;
+	char option;
+	list mylist;
+	cout << "(a) -- insert.		";
+	cout << "(b) -- insertat.	";
+	cout << "(c) -- delete.		";
+	cout << "(d) -- deleteAt.	";
+	cout << "(e) -- countItems.	";
+	cout << "(f) -- dislpay.	";
+	cout << "(g) -- exit	";
+	while (option != 'g')
+	{
+		cin >> option;
+		if(option=='a')
+		{
+			cout << "Enter Value";
+			cin >> value;
+			mylist.insert(value);
+		}
+		else if(option=='b')
+		{
+			cout << "Enter Value";
+			cin >> value;
+			cout << "Enter position";
+			cin >> position;
+			mylist.insertat(position,value);
+		}
+		else if(option=='c')
+			mylist.del();
+		else if(option=='d')
+		{
+			cout << "Enter position";
+			cin >> position;
+			mylist.deleteat(position);
+		}
+		else if(option=='e')
+			mylist.countItems();
+		else if(option=='f')
+			mylist.display();
+		else if(option== 'g')
+			cout << "Bye";
+		else
+			cout << "Invalid option";
+	}
 return 0;
-}
-
-void link :: insert_list(int data)
-{
-     node *temp = new node;
-     temp->data = data;
-     if (head == NULL)
-     {
-         head=temp;
-         tail=temp;
-         temp->next = NULL;
-     }
-     else
-        {
-           tail->next = temp;
-           temp->next = NULL;
-           tail=temp;
-        }
-}
-
-void link :: insert_at(int pos, int dat)
-{
-    node *tmp =new node;
-    node *pos;
-    pos=head;
-    tmp->data= dat;
-    for (int i=2;i<pos;i++)
-    {
-        pos=pos->next;
-    }
-    tmp->next = pos->next;
-    pos->next = tmp;
-}
-
-void link :: delete_list()
-{
-    node *pos;
-    pos=head;
-    while (pos->next != tail)
-    {
-        pos=pos->next;
-    }
-    tail=pos;
-    pos=pos->next;
-    tail->next = NULL;
-    delete pos;
-
-}
-
-void link :: delete_at(int post)
-{ node *pos;
-  pos=head;
-  for (int i=2;i<post;i++)
-  {
-      pos=pos->next;
-  }
-  node *temp;
-  temp = pos->next;
-  pos->next = temp->next;
-  temp->next = NULL;
-  delete temp;
-}
-
-void link :: display_l()
-{
-    node *pos;
-    pos=head;
-    cout<<pos->data<<" -> ";
-    while (pos->next = NULL)
-    {
-        pos=pos->next;
-        cout<<pos->data<<" -> ";
-    }
-}
-
-void link :: count_l()
-{
-
 }
